aa75988da5465e2c4929273bfabbe72492ed0598
bladedurman <vedantvgrover@gmail.com>
2022-08-21 09:35:23 -0700

    Fixing work check bugs

--- src/main/java/com/freyr/apollo18/Apollo18.java
+++ src/main/java/com/freyr/apollo18/Apollo18.java
@@ -57,7 +57,7 @@ public class Apollo18 {
         shardManager.addEventListener(new GuildListener(this), new ButtonListener(), new LevelingListener(this), new CommandManager(this), new BotListener(this));
 
         ZonedDateTime now = ZonedDateTime.now(ZoneId.of("America/Los_Angeles"));
-        ZonedDateTime nextRun = now.withHour(23).withMinute(59).withSecond(0);
+        ZonedDateTime nextRun = now.withHour(0).withMinute(0).withSecond(0);
         if(now.compareTo(nextRun) > 0) {
             nextRun = nextRun.plusDays(1);
         }
@@ -67,9 +67,10 @@ public class Apollo18 {
 
         ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
         scheduler.scheduleAtFixedRate(() -> {
+            System.out.println("Running");
             database.updateStocks();
             database.dailyWorkChecks();
-        }, initialDelay, TimeUnit.DAYS.toSeconds(1), TimeUnit.SECONDS);
+        }, 5, 5, TimeUnit.SECONDS);
 
         System.out.println("Will run at " + nextRun.format(DateTimeFormatter.ofPattern("yyyy/MM/dd-hh:mm:ss")));
     }
--- src/main/java/com/freyr/apollo18/data/Database.java
+++ src/main/java/com/freyr/apollo18/data/Database.java
@@ -803,7 +803,7 @@ public class Database {
     public void dailyWorkChecks() {
         for (Document user : userData.find()) {
             Document query = new Document("userID", user.getString("userID"));
-            if (user.get("economy", Document.class).get("job", Document.class).getInteger("daysMissed") == null) {
+            if (user.get("economy", Document.class).get("job", Document.class).getInteger("daysMissed") == null || user.get("economy", Document.class).get("job", Document.class).getBoolean("worked") == null) {
                 Document job = new Document("business", null).append("job", null).append("daysWorked", 0).append("daysMissed", 0).append("worked", false);
                 Bson updates = Updates.set("economy.job", job);
 
