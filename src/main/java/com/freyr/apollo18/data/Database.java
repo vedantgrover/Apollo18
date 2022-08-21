56b855205ce604dfabd8d4b94e7bec157f8ebee8
bladedurman <vedantvgrover@gmail.com>
2022-08-21 10:38:01 -0700

    Putting work checks into testing

--- src/main/java/com/freyr/apollo18/data/Database.java
+++ src/main/java/com/freyr/apollo18/data/Database.java
@@ -804,12 +804,14 @@ public class Database {
     public void dailyWorkChecks() {
         for (Document user : userData.find(new Document())) {
             Document query = new Document("userID", user.getString("userID"));
-            if (user.get("economy", Document.class).get("job", Document.class).getBoolean("worked") == null) {
+            if (user.get("economy", Document.class).get("job", Document.class).getInteger("daysMissed") == null
+                    || user.get("economy", Document.class).get("job", Document.class).getBoolean("worked") == null) {
                 Document job = new Document("business", null).append("job", null).append("daysWorked", 0).append("daysMissed", 0).append("worked", false);
                 Bson updates = Updates.set("economy.job", job);
 
                 userData.updateOne(query, updates, new UpdateOptions().upsert(true));
                 System.out.println("Replaced Job Data with Updated Job Data for " + user.getString("userID"));
+                System.out.println(user.get("economy", Document.class).get("job", Document.class));
             }
             if (!user.get("economy", Document.class).get("job", Document.class).getBoolean("worked") && user.get("economy", Document.class).get("job", Document.class).getString("job") != null) {
                 Bson updates = Updates.combine(
