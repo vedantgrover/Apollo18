465365a38aedacbab835ccbe1beea450fc28d41f
vedantgrover <68160833+vedantgrover@users.noreply.github.com>
2023-05-15 16:40:22 -0700

    Removes functionality from binary commands

--- src/main/java/com/freyr/apollo18/commands/fun/BinaryCommand.java
+++ src/main/java/com/freyr/apollo18/commands/fun/BinaryCommand.java
@@ -8,6 +8,8 @@ import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEve
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 
+import java.net.URI;
+
 public class BinaryCommand extends Command {
 
     public BinaryCommand(Apollo18 bot) {
@@ -24,20 +26,22 @@ public class BinaryCommand extends Command {
     public void execute(SlashCommandInteractionEvent event) {
         event.deferReply().queue();
 
-        String subCommand = event.getSubcommandName();
+        event.getHook().sendMessageEmbeds(EmbedUtils.createNotification("Currently under repair")).queue();
 
-        String data;
-
-        switch (subCommand) {
-            case "encode":
-                data = getApiData("https://some-random-api.ml/binary?text=" + event.getOption("string").getAsString()).getString("binary");
-                event.getHook().sendMessage("```" + data + "```").queue();
-            break;
-
-            case "decode":
-                data = getApiData("https://some-random-api.ml/binary?decode=" + event.getOption("string").getAsString()).getString("text");
-                event.getHook().sendMessage("```" + data + "```").queue();
-            break;
-        }
+//        String subCommand = event.getSubcommandName();
+//
+//        String data;
+//
+//        switch (subCommand) {
+//            case "encode":
+//                data = getApiData("https://some-random-api.ml/binary?text=" + URI.create(event.getOption("string").getAsString())).getString("binary");
+//                event.getHook().sendMessage("```" + data + "```").queue();
+//            break;
+//
+//            case "decode":
+//                data = getApiData("https://some-random-api.ml/binary?decode=" + event.getOption("string").getAsString()).getString("text");
+//                event.getHook().sendMessage("```" + data + "```").queue();
+//            break;
+//        }
     }
 }
