17b035e7a7b70ecc21b92af4198877c49255cd0e
vedantgrover <68160833+vedantgrover@users.noreply.github.com>
2023-05-19 20:22:17 -0700

    Adding new byte emoji ID

--- src/main/java/com/freyr/apollo18/handlers/BusinessHandler.java
+++ src/main/java/com/freyr/apollo18/handlers/BusinessHandler.java
@@ -5,7 +5,7 @@ public class BusinessHandler {
     private static final String upArrow = "<:increase:1107148093830471681>";
     private static final String downArrow = "<:decrease:1107148133496004748>";
     private static final String neutral = "<:no_change:1107057693627269171>";
-    public static final String byteEmoji = "";
+    public static final String byteEmoji = "<:byte:1109318930901766224>";
 
     public static String getArrow(int change) {
         if (change == 0) {
