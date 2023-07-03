09c7966c14cdc2a44e7be97059db52aa901204d0
vedantgrover <68160833+vedantgrover@users.noreply.github.com>
2023-07-02 17:35:25 -0700

    Removing outdated server invite

--- src/main/java/com/freyr/apollo18/commands/utility/InviteCommand.java
+++ src/main/java/com/freyr/apollo18/commands/utility/InviteCommand.java
@@ -31,7 +31,7 @@ public class InviteCommand extends Command {
         botEmbed.setThumbnail("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/53653317-d541-48a0-8252-bc533c980e00/ddk20gg-5d876f13-a2e3-4a64-a589-45e1cee6ee40.png/v1/fill/w_600,h_835,strp/apollo___greek_mythology_by_yliade_ddk20gg-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzUzNjUzMzE3LWQ1NDEtNDhhMC04MjUyLWJjNTMzYzk4MGUwMFwvZGRrMjBnZy01ZDg3NmYxMy1hMmUzLTRhNjQtYTU4OS00NWUxY2VlNmVlNDAucG5nIiwiaGVpZ2h0IjoiPD04MzUiLCJ3aWR0aCI6Ijw9NjAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLndhdGVybWFyayJdLCJ3bWsiOnsicGF0aCI6Ilwvd21cLzUzNjUzMzE3LWQ1NDEtNDhhMC04MjUyLWJjNTMzYzk4MGUwMFwveWxpYWRlLTQucG5nIiwib3BhY2l0eSI6OTUsInByb3BvcnRpb25zIjowLjQ1LCJncmF2aXR5IjoiY2VudGVyIn19.kJ-P4pwq5NVxQWBqvl_x7s4OQqKyp5GB7202NMCuxP4");
         botEmbed.setColor(EmbedColor.DEFAULT_COLOR);
         botEmbed.setDescription("Thank you for taking an interest in sharing this bot! Apollo is a multi-purpose bot and is currently in development! Important links are below.");
-        botEmbed.addField("Server Invite", "https://discord.gg/zqK2PXAuyk", false);
+        botEmbed.addField("Server Invite", "Coming soon...", false);
         botEmbed.addField("Bot Invite", "Invite Apollo18 [here](https://discord.com/api/oauth2/authorize?client_id=853812538218381352&permissions=8&scope=applications.commands%20bot)!", true);
         botEmbed.setFooter("Requested by " + event.getUser().getName());
         event.getHook().sendMessageEmbeds(botEmbed.build()).queue();
