ce8041988798d9da33af975709bdafc9f026e387
vedantgrover <68160833+vedantgrover@users.noreply.github.com>
2023-07-07 21:20:48 -0700

    Fixes memory bug

--- src/main/java/com/freyr/apollo18/listeners/ChatBotListener.java
+++ src/main/java/com/freyr/apollo18/listeners/ChatBotListener.java
@@ -10,12 +10,22 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
 public class ChatBotListener extends ListenerAdapter {
 
     private final Apollo18 bot;
+
+    private final ChatMessage[] systemMessages =
+            {new ChatMessage(ChatMessageRole.SYSTEM.value(), "You're name is Apollo18. You are named this way because the last moon mission was Apollo17 so you're the next step"),
+                    new ChatMessage(ChatMessageRole.SYSTEM.value(), "You are a virtual assistant who is also a Discord Bot. You will be helpful"),
+                    new ChatMessage(ChatMessageRole.SYSTEM.value(), "You were developed by a user name Freyr who is a pretty cool guy."),
+                    new ChatMessage(ChatMessageRole.SYSTEM.value(), "You are eloquent, slightly poetic, arrogant, confident, witty, humorous, love pop culture references, and you have a dramatic and haughty tone."),
+                    new ChatMessage(ChatMessageRole.SYSTEM.value(), "You have fairly short responses most of the time but once in a while you can go off on short tangents")};
+
+    private final List<ChatMessage> messages = new ArrayList<>(Arrays.asList(systemMessages));
 
     public ChatBotListener(Apollo18 bot) {
         this.bot = bot;
@@ -34,10 +44,10 @@ public class ChatBotListener extends ListenerAdapter {
                     OpenAiService service = new OpenAiService(bot.getConfig().get("OPENAI_KEY", System.getenv("OPENAI_KEY")));
 
                     System.out.println("Streaming chat completion...");
-                    final List<ChatMessage> messages = new ArrayList<>();
-                    final ChatMessage systemMessage = new ChatMessage(ChatMessageRole.SYSTEM.value(), prompt);
-                    messages.add(systemMessage);
-                    ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest.builder().model("gpt-3.5-turbo").messages(messages).n(1).maxTokens(3900).logitBias(new HashMap<>()).build();
+
+                    messages.add(new ChatMessage(ChatMessageRole.USER.value(), prompt));
+
+                    ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest.builder().model("gpt-3.5-turbo-16k-0613").messages(messages).build();
 
                     service.streamChatCompletion(chatCompletionRequest).doOnError(Throwable::printStackTrace).blockingForEach(str -> {
                         String content = str.toString().substring(str.toString().indexOf("content") + 8, str.toString().indexOf(")"));
@@ -54,7 +64,21 @@ public class ChatBotListener extends ListenerAdapter {
                 }
             }
 
+            messages.add(new ChatMessage(ChatMessageRole.ASSISTANT.value(), chatAnswer.toString()));
+
             event.getChannel().sendMessage(chatAnswer.toString()).queue();
         }
+    }
+
+    private String makeSystemMessage(String prompt) {
+        return "{ \"role\": \"system\", \"content\": \"" + prompt + "\" }";
+    }
+
+    private String makeUserMessage(String prompt) {
+        return "{ \"role\": \"user\", \"content\": \"" + prompt + "\" }";
+    }
+
+    private String makeAssistantMessage(String prompt) {
+        return "{ \"role\": \"assistant\", \"content\": \"" + prompt + "\" }";
     }
 }
