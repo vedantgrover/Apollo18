923545e0140852823ed15d0767b58f3da6eea125
vedantgrover <68160833+vedantgrover@users.noreply.github.com>
2023-07-23 12:19:03 -0700

    Adds stock lookup command

--- src/main/java/com/freyr/apollo18/handlers/BusinessHandler.java
+++ src/main/java/com/freyr/apollo18/handlers/BusinessHandler.java
@@ -2,9 +2,9 @@ package com.freyr.apollo18.handlers;
 
 public class BusinessHandler {
 
-    private static final String upArrow = "<:increase:1107148093830471681>";
-    private static final String downArrow = "<:decrease:1107148133496004748>";
-    private static final String neutral = "<:no_change:1107057693627269171>";
+    public static final String upArrow = "<:increase:1107148093830471681>";
+    public static final String downArrow = "<:decrease:1107148133496004748>";
+    public static final String neutral = "<:no_change:1107057693627269171>";
     public static final String byteEmoji = "<:byte:1109318930901766224>";
 
     public static String getArrow(int change) {
