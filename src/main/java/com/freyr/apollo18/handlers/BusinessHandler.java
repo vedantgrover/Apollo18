8f3c7b26da623bcadc23153aad9cc86d5b23ff8b
Vedant Grover <vedantvgrover@gmail.com>
2025-06-13 12:21:31 -0700

    Add correct emoji ids

--- src/main/java/com/freyr/apollo18/handlers/BusinessHandler.java
+++ src/main/java/com/freyr/apollo18/handlers/BusinessHandler.java
@@ -2,10 +2,10 @@ package com.freyr.apollo18.handlers;
 
 public class BusinessHandler {
 
-    public static final String upArrow = "<:increase:1377821574509367396>";
-    public static final String downArrow = "<:decrease:1377821567995744266>";
-    public static final String neutral = "<:no_change:1377822916347170956>";
-    public static final String byteEmoji = "<:byte:1377825496003379330>";
+    public static final String upArrow = "<:increase:1378007802404012063>";
+    public static final String downArrow = "<:decrease:1378007776546127882>";
+    public static final String neutral = "<:same:1378007830648324176>";
+    public static final String byteEmoji = "<:byte:1378007816861782255>";
 
     public static String getArrow(int change) {
         if (change == 0) {
